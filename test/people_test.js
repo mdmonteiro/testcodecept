f8f1497da24d6e34448c296a8af56c9af8a1c70b
mdmonteiro <mmonteiroico@gmail.com>
2020-02-23 16:28:25 -0300

    Melhoria no código

--- test/people_test.js
+++ test/people_test.js
@@ -5,48 +5,17 @@ Feature('API One - People');
 
 Scenario('Teste Status Code', async () => {
     var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.status).to.eql(200);
-
+    expect(res.status).to.eql(200)
 });
 
-Scenario('Validação Name', async () => {
+Scenario('Validação Data JSON', async () => {
     var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.name).to.eql('Luke Skywalker');
-
-});
-
-Scenario('Validação Height', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.height).to.eql('172');
-
-});
-
-Scenario('Validação Hair_color', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.hair_color).to.eql('blond');
-
-});
-
-Scenario('Validação Skin_color', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.skin_color).to.eql('fair');
-
-});
-
-Scenario('Validação Eye_color', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.eye_color).to.eql('blue');
-
-});
-
-Scenario('Validação Birth_year', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
-    expect(res.data.birth_year).to.eql('19BBY');
-
-});
-
-Scenario('Validação Gender', async () => {
-    var res = await I.sendGetRequest('/api/people/1/');
+    expect(res.data.name).to.eql('Luke Skywalker')
+    expect(res.data.height).to.eql('172')
+    expect(res.data.hair_color).to.eql('blond')
+    expect(res.data.skin_color).to.eql('fair')
+    expect(res.data.eye_color).to.eql('blue')
+    expect(res.data.birth_year).to.eql('19BBY')
     expect(res.data.gender).to.eql('male');
 
 });
--- test/planet_test.js
+++ test/planet_test.js
@@ -9,50 +9,15 @@ Scenario('Teste Status Code', async () => {
 
 });
 
-Scenario('Validação Name', async () => {
+Scenario('Validação Data JSON', async () => {
     var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.name).to.eql('Tatooine');
-
-});
-
-Scenario('Validação Rotation_period', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.rotation_period).to.eql('23');
-
-});
-
-Scenario('Validação Orbital_period', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.orbital_period).to.eql('304');
-
-});
-
-Scenario('Validação Climate', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.climate).to.eql('arid');
-
-});
-
-Scenario('Validação Gravity', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.gravity).to.eql('1 standard');
-
-});
-
-Scenario('Validação Terrain', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.terrain).to.eql('desert');
-
-});
-
-Scenario('Validação Surface_water', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
-    expect(res.data.surface_water).to.eql('1');
-
-});
-
-Scenario('Validação Population', async () => {
-    var res = await I.sendGetRequest('/api/planets/1/');
+    expect(res.data.name).to.eql('Tatooine')
+    expect(res.data.rotation_period).to.eql('23')
+    expect(res.data.orbital_period).to.eql('304')
+    expect(res.data.climate).to.eql('arid')
+    expect(res.data.gravity).to.eql('1 standard')
+    expect(res.data.terrain).to.eql('desert')
+    expect(res.data.surface_water).to.eql('1')
     expect(res.data.population).to.eql('200000');
 
 });
